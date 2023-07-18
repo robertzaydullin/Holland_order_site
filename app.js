8fd076873e0f5f0fc61bdef5d97155ab0073316a
robertzaydullin <136914692+robertzaydullin@users.noreply.github.com>
2023-07-18 11:53:34 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -12,74 +12,147 @@ let timeChoise = document.getElementById('getorder-time')
 timeChoise.min = new Date().toISOString().slice(0, -8)
 
 let shops = {
-	'АМИ': [[55.829140, 49.133510], 'ул. Ф. Амирхана, 10', 'Казань, Ново-Савиновский район'],
-	'БАУ': [[55.7926, 49.110168], 'ул. Баумана, 38/17', 'Казань, Вахитовский район']
-/*	
-	'БЕЛ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.101015%2C55.861308&mode=search&oid=28202707539&ol=biz&sll=49.101015%2C55.861308&text=holland&z=16',	
-	'БИГ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.254561%2C55.781496&mode=search&oid=97456772243&ol=biz&sll=49.254561%2C55.781496&text=holland&z=16',	
-	'ВИШ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.146303%2C55.785132&mode=search&oid=72178542010&ol=biz&sll=49.146303%2C55.785132&text=holland&z=16',	
-	'ВОС': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.068592%2C55.832345&mode=search&oid=90464841972&ol=biz&sll=49.068592%2C55.832345&text=holland&z=16',	
-	'ГАГ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.089160%2C55.835726&mode=search&oid=18603392162&ol=biz&sll=49.089160%2C55.835726&text=holland&z=16',	
-	'ГВА': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.170906%2C55.788722&mode=search&oid=213016406099&ol=biz&sll=49.170906%2C55.788722&text=holland&z=16',
-	'ГОР': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.138544%2C55.794074&mode=search&oid=132785029633&ol=biz&sll=49.138544%2C55.794074&text=holland&z=16',
-	'ГУБ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.190720%2C55.805465&mode=search&oid=185140938451&ol=biz&sll=49.190720%2C55.805465&text=holland&z=16',
-	'Д40': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.180103%2C55.764762&mode=search&oid=163532602468&ol=biz&sll=49.180103%2C55.764762&text=holland&z=16',
-	'ДЕК': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.089998%2C55.820134&mode=search&oid=220734194938&ol=biz&sll=49.089998%2C55.820134&text=holland&z=16',
-	'ДОС': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.154793%2C55.788111&mode=search&oid=237644359130&ol=biz&sll=49.154793%2C55.788111&text=holland&z=16',
-	'ЕРШ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.183154%2C55.800494&mode=search&oid=111702055150&ol=biz&sll=49.183154%2C55.800494&text=holland&z=16',
-	'ЗОР': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.222299%2C55.742885&mode=search&oid=119379299168&ol=biz&sll=49.222299%2C55.742885&text=holland&z=16',
-	'КЛН': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.147003%2C55.783040&mode=search&oid=23366454746&ol=biz&sll=49.147003%2C55.783040&text=holland&z=16',
-	'КОС': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.188066%2C55.801566&mode=search&oid=217270655070&ol=biz&sll=49.188066%2C55.801566&text=holland&z=16',
-	'ЛЕН': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.085731%2C55.862304&mode=search&oid=22469840907&ol=biz&sll=49.085731%2C55.862304&text=holland&z=16',
-	'ЛОМ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.230397%2C55.767172&mode=search&oid=228768362208&ol=biz&sll=49.230397%2C55.767172&text=holland&z=16',
-	'ЛУК': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.090188%2C55.861954&mode=search&oid=28888710249&ol=biz&sll=49.090188%2C55.861954&text=holland&z=16',
-	'МАК': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.079574%2C55.862919&mode=search&oid=167789163140&ol=biz&sll=49.079574%2C55.862919&text=holland&z=16',
-	'МРС': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.137647%2C55.795269&mode=search&oid=157134491588&ol=biz&sll=49.137647%2C55.795269&text=holland&z=16',
-	'МСК': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.084519%2C55.830593&mode=search&oid=74911952640&ol=biz&sll=49.084519%2C55.830593&text=holland&z=16',
-	'ПАВ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.148431%2C55.766649&mode=search&oid=66495086038&ol=biz&sll=49.148431%2C55.766649&text=holland&z=16',
-	'ПАР': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.189769%2C55.745739&mode=search&oid=29910588982&ol=biz&sll=49.189769%2C55.745739&text=holland&z=16',		
-	'ПИО': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.185149%2C55.805840&mode=search&oid=103392130342&ol=biz&sll=49.185149%2C55.805840&text=holland&z=16',		
-	'ПОБ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.220710%2C55.768300&mode=search&oid=112890755875&ol=biz&sll=49.220710%2C55.768300&text=holland&z=16',		
-	'ПШК': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.122976%2C55.786570&mode=search&oid=1645643275&ol=biz&sll=49.122976%2C55.786570&text=holland&z=16',
-	'САВ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.150096%2C55.827630&mode=search&oid=29136626608&ol=biz&sll=49.150096%2C55.827630&text=holland&z=16',
-	'САХ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.233613%2C55.780401&mode=search&oid=85712801621&ol=biz&sll=49.233613%2C55.780401&text=holland&z=16',
-	'СИБ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.182654%2C55.809970&mode=search&oid=1465762934&ol=biz&sll=49.182654%2C55.809970&text=holland&z=16',
-	'СЛВ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=48.907657%2C55.855640&mode=search&oid=85591292351&ol=biz&sll=48.907657%2C55.855640&text=holland&z=16',
-	'ТАТ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.116853%2C55.782212&mode=search&oid=219382741699&ol=biz&sll=49.116853%2C55.782212&text=holland&z=16',
-	'УНИ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.123437%2C55.790145&mode=search&oid=77882752607&ol=biz&sll=49.123437%2C55.790145&text=holland&z=16',
-	'Ф106': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.232375%2C55.769707&mode=search&oid=213883763512&ol=biz&sll=49.232375%2C55.769707&text=holland&z=16',
-	'ФУЧ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.232357%2C55.755849&mode=search&oid=211680143113&ol=biz&sll=49.232357%2C55.755849&text=holland&z=16',
-	'Ч15': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.106065%2C55.819481&mode=search&oid=101046037813&ol=biz&sll=49.106065%2C55.819481&text=holland&z=16',
-	'ЧЕТ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.121416%2C55.824597&mode=search&oid=22377431664&ol=biz&sll=49.121416%2C55.824597&text=holland&z=16',
-	'ЧЕХ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.147576%2C55.789670&mode=search&oid=68683820458&ol=biz&sll=49.147576%2C55.789670&text=holland&z=16',
-	'ЧИС': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.100406%2C55.819285&mode=search&oid=146226951517&ol=biz&sll=49.100406%2C55.819285&text=holland&z=16',
-	'ЯПЕ': 'https://yandex.ru/map-widget/v1/?filter=chain_id%3A190469064514&indoorLevel=1&ll=49.116204%2C55.798754&mode=search&oid=221947341800&ol=biz&sll=49.116204%2C55.798754&text=holland&z=16',
-*/
+	'АМИ': [[55.82862, 49.13423], 'ул. Ф. Амирхана, 10', 'Казань, Ново-Савиновский район'],
+	'БАУ': [[55.7926, 49.110168], 'ул. Баумана, 38/17', 'Казань, Вахитовский район'],
+	'БЕЛ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'БИГ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ВИШ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ВОС': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ГАГ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ГВА': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ГОР': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ГУБ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'Д40': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ДЕК': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ДОС': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЕРШ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЗОР': [[55.7926, 49.110168], 'адрес', 'район'],
+	'КЛН': [[55.7926, 49.110168], 'адрес', 'район'],
+	'КОС': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЛЕН': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЛОМ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЛУК': [[55.7926, 49.110168], 'адрес', 'район'],
+	'МАК': [[55.7926, 49.110168], 'адрес', 'район'],
+	'МРС': [[55.7926, 49.110168], 'адрес', 'район'],
+	'МСК': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ПАВ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ПАР': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ПИО': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ПОБ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ПШК': [[55.786776, 49.121034], 'ул. Пушкина, 5', 'Казань, Вахитовский район'],
+	'САВ': [[55.826703, 49.147909], 'просп. Ямашева, 93, корп. 1', 'Казань, Ново-Савиновский район'],
+	'САХ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'СИБ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'СЛВ': [[55.854902, 48.899283], 'ул. Наиля Юсупова, 9','Казань, Кировский район'],
+	'ТАТ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'УНИ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'Ф106': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ФУЧ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'Ч15': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЧЕХ': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЧИС': [[55.7926, 49.110168], 'адрес', 'район'],
+	'ЯПЕ': [[55.7926, 49.110168], 'адрес', 'район']
 }
 
+let area = []
+let optgr =  []
+let shopList = []
+for (key in shops){
+	shopInfo = shops[key]
+
+	let findNumber = area.indexOf(shopInfo[2])
+	if (findNumber == -1) {
+		groupName = shopInfo[2]
+		groupName = document.createElement("optgroup")
+		groupName.label = shopInfo[2]
+		area.push(shopInfo[2])
+		optgr.push(groupName)
+	} else {
+		groupName = optgr[findNumber]
+	}
+	
+	shopName = key
+	shopList.push(shopName)
+	key = document.createElement("option")
+	key.value = shopName
+	key.text = shopInfo[1]
+	groupName.appendChild(key)
+	shopChoice.appendChild(groupName)
+}	
+
+
+
 DG.then(function () {
-   let map
+   let map, myLocation
    map = DG.map('map', {
    center: [55.83, 49.05],
    zoom: 10,
+   minZoom: 10,
    fullscreenControl: false
    })
 
-   DG.marker(shops['АМИ'][0])
-      	.on('click', function() {
-	shopChoice.value = 'АМИ'
-	})
-     	.addTo(map)
-	.bindLabel(shops['АМИ'][1])
+   let findButton = document.getElementById('findme');
 
-   DG.marker(shops['БАУ'][0])
-      	.on('click', function() {
-	shopChoice.value = 'БАУ'
-	})
-     	.addTo(map)
-	.bindLabel(shops['БАУ'][1])
+   findButton.onclick = function() {myLocation};
+
+   myLocation = map.locate({setView: true, watch: true})
+   		.on('locationfound', function(e) {
+	   		DG.marker([e.latitude, e.longitude]).addTo(map);
+   			})
+   		.on('locationerror', function(e) {
+	   		DG.popup()
+		 	.setLatLng(map.getCenter())
+		 	.setContent('Доступ к определению местоположения отключён')
+		 	.openOn(map);
+   			})
+
+	shopChoice.onchange = function changeShop () {
+		map.setView(center=shops[shopChoice.value][0], zoom=16)
+	tg.MainButton.show()
+}
 
 
+
+
+	DG.marker(shops['ПШК'][0])
+		.on('click', function() {
+			shopChoice.value = 'ПШК'
+			tg.MainButton.show()
+		})
+		.addTo(map)
+		.bindLabel(shops['ПШК'][1])
+
+	DG.marker(shops['САВ'][0])
+		.on('click', function() {
+			shopChoice.value = 'САВ'
+			tg.MainButton.show()
+		})
+		.addTo(map)
+		.bindLabel(shops['САВ'][1])
+
+	DG.marker(shops['АМИ'][0])
+		.on('click', function() {
+			shopChoice.value = 'АМИ'
+			tg.MainButton.show()
+		})
+		.addTo(map)
+		.bindLabel(shops['АМИ'][1])
+
+	DG.marker(shops['БАУ'][0])
+		.on('click', function() {
+			shopChoice.value = 'БАУ'
+			tg.MainButton.show()
+		})
+		.addTo(map)
+		.bindLabel(shops['БАУ'][1])
+
+	DG.marker(shops['СЛВ'][0])
+		.on('click', function() {
+			shopChoice.value = 'СЛВ'
+			tg.MainButton.show()
+		})
+		.addTo(map)
+		.bindLabel(shops['СЛВ'][1])
 
 })
 
@@ -87,16 +160,6 @@ DG.then(function () {
 
 
 
-
-shopChoice.onchange = function changeShop () {
-	alert('here2')	
-	tg.MainButton.show()
-}
-
-function changeShop() {
-	alert('here2')	
-	tg.MainButton.show()
-}
 
 
 
